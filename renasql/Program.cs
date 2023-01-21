55fe021a75b323c7a60f080fa52bc0f50b3e44f7
Ace_Radom <sichenradomlyu@gmail.com>
2023-01-21 22:44:38 +0100

    upload renasql base

---
+++ renasql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace renasql
+{
+    internal class renasql_main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "RenaSQL CLI version " + sv.major.ToString() + "." + sv.minor.ToString() + "." + sv.patch.ToString() + "\n" +
+                "Enter \".help\" for usage hints" + "\n"
+            );
+        }
+    }
+}
---
+++ renasql/sv.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace renasql
+{
+    /// <summary>
+    /// Software Version Class
+    /// </summary>
+    internal class sv
+    {
+        public const UInt16 major = 0;
+        public const UInt16 minor = 1;
+        public const UInt16 patch = 0;
+    }
+}
